6d08f054d7a3d538d3fcc97f41d11aae5ed15f56
GAMER <GAMER@DESKTOP-I2KEC4H>
2020-11-18 19:05:33 -0300

    Paltitest

---
+++ src/main/java/com/mycompany/paltitest/MainPalta.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.paltitest;
+
+/**
+ *
+ * @author GAMER
+ */
+public class MainPalta {
+    
+    
+    
+    public static void main (String[]args){
+    
+    
+    
+    
+    }
+    
+}
