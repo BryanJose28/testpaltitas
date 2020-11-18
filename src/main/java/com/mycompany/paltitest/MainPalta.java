6f43e9bcc9ad011d0f515cdea1aa7d6f43a81edc
GAMER <GAMER@DESKTOP-I2KEC4H>
2020-11-18 19:24:04 -0300

    Paltitest

--- src/main/java/com/mycompany/paltitest/MainPalta.java
+++ src/main/java/com/mycompany/paltitest/MainPalta.java
@@ -15,8 +15,8 @@ public class MainPalta {
     
     public static void main (String[]args){
     
-    
-    
+    System.out.println("Hola mundo :3");
+    new MainMenu().setVisible(true);
     
     }
     
